3f070cf9f1763eb4f8871eafec1576a80197a7fa
rodiontkachenko <rodion.tkachenko31@GMAIL.COM>
2026-05-08 13:31:19 +0300

    new table + auth from this table creds

---
+++ src/main/java/web/development/blog/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package web.development.blog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import web.development.blog.model.User;
+import web.development.blog.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + username));
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+    }
+}
